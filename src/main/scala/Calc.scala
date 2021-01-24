e9668c159dc315ad222c9b0501d3c645e580708d
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2021-01-25 06:17:18 +0900

    test: use scalatest

--- src/main/scala/Calc.scala
+++ src/main/scala/Calc.scala
@@ -1,8 +1,13 @@
 class Calc {
 
-  def sum():Int = ???
+  def sum(seq: Seq[Int]):Int = seq.foldLeft(0)(_ + _)
 
-  def div(): Double = ???
+  def div(numerator: Int, denominator: Int): Double = {
+    if (denominator == 0) throw new ArithmeticException("divide by zero.")
+    numerator.toDouble / denominator.toDouble
+  }
 
-  def isPrime(): Boolean = ???
+  def isPrimeNumber(n: Int): Boolean = {
+    if (n < 2) false else !((2 to Math.sqrt(n).toInt) exists (n % _ == 0))
+  }
 }
---
+++ src/test/scala/CalcSpec.scala
@@ -0,0 +1,41 @@
+import org.scalatest._
+import org.scalatest.concurrent.Timeouts
+import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
+
+class CalcSpec extends FlatSpec with DiagrammedAssertions with Timeouts {
+
+  val sut = new Calc()
+
+  "sum function" should "Standard" in {
+    assert(sut.sum(Seq(0, 2, 4)) === 6)
+    assert(sut.sum(Seq()) === 0)
+  }
+
+  it should "Overflow when over the max integer" in {
+    assert(sut.sum(Seq(Integer.MAX_VALUE, 1)) === Integer.MIN_VALUE)
+  }
+
+  "div function" should "Standard" in {
+    assert(sut.div(4, 2) === 2.0)
+  }
+
+  it should "Exception when divide by zero" in {
+    intercept[ArithmeticException] {
+      sut.div(4, 0)
+    }
+  }
+
+  "isPrimeNumber function" should "Standard" in {
+    assert(sut.isPrimeNumber(-1) === false)
+    assert(sut.isPrimeNumber(0) === false)
+    assert(sut.isPrimeNumber(1) === false)
+    assert(sut.isPrimeNumber(2))
+    assert(sut.isPrimeNumber(11))
+  }
+
+  it should "time" in {
+    failAfter(1000.millis) {
+      assert(sut.isPrimeNumber(999999) === false)
+    }
+  }
+}
